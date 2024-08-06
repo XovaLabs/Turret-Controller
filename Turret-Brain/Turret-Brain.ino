bd06ab254879e2356f1df09b479048fad2b955b4
Carlos Rodriguez <rodrigcp22@gmail.com>
2024-08-06 14:44:05 +1000

    Create Class Instance
    The whole project was changed from solitary functions to a single class to streamline the process as well as set the base of the pyramid.

--- Turret-Brain/Turret-Brain.ino
+++ Turret-Brain/Turret-Brain.ino
@@ -1,94 +1,118 @@
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include <LittleFS.h>
 #include <WebSocketsServer.h>
 
-// Replace with your network credentials
-const char* ssid = "Your_SSID";
-const char* password = "Your_Password";
+class WebServerManager {
+public:
+    WebServerManager(const char* ssid, const char* password)
+    : ssid(ssid), password(password), server(80), webSocket(81) {}
 
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
-WebSocketsServer webSocket = WebSocketsServer(81);
+    void begin() {
+        // Serial port for debugging purposes
+        Serial.begin(115200);
 
-// Replaces placeholder with LED state value
-String processor(const String& var){
-  if(var == "STATE"){
-    return "0";
-  }
-  return String();
+        // Initialize LittleFS
+        if (!LittleFS.begin()) {
+            Serial.println("An Error has occurred while mounting LittleFS");
+            return;
+        }
+
+        // Connect to Wi-Fi
+        WiFi.begin(ssid, password);
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(1000);
+            Serial.println("Connecting to WiFi...");
+        }
+        Serial.println("Connected to WiFi");
+        Serial.println(WiFi.localIP());
+
+        // Set up server routes
+        setupServerRoutes();
+
+        // Start server
+        server.begin();
+
+        // Start WebSocket server
+        webSocket.begin();
+        webSocket.onEvent(std::bind(&WebServerManager::onWebSocketEvent, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4));
+    }
+
+    void loop() {
+        webSocket.loop();
+    }
+
+private:
+    const char* ssid;
+    const char* password;
+    AsyncWebServer server;
+    WebSocketsServer webSocket;
+
+    void setupServerRoutes() {
+        // Route for root / web page
+        server.on("/", HTTP_GET, [this](AsyncWebServerRequest *request) {
+            request->send(LittleFS, "/jerk.html", String(), false, std::bind(&WebServerManager::processor, this, std::placeholders::_1));
+        });
+
+        // Route to load JavaScript file
+        server.on("/joy.js", HTTP_GET, [this](AsyncWebServerRequest *request) {
+            request->send(LittleFS, "/joy.js", "text/javascript");
+        });
+
+        // Route to load minified JavaScript file
+        server.on("/joy.min.js", HTTP_GET, [this](AsyncWebServerRequest *request) {
+            request->send(LittleFS, "/joy.min.js", "text/javascript");
+        });
+
+        server.onNotFound(std::bind(&WebServerManager::notFound, this, std::placeholders::_1));
+    }
+
+    String processor(const String& var) {
+        if (var == "STATE") {
+            return "0";
+        }
+        return String();
+    }
+
+    void notFound(AsyncWebServerRequest *request) {
+        request->send(404, "text/plain", "Not found");
+    }
+
+    void handleWebSocketMessage(uint8_t num, uint8_t *data, size_t len) {
+        data[len] = 0; // Null-terminate the string
+        Serial.printf("WebSocket Message: %s\n", (char *)data);
+
+        // Send a response back to the client
+        webSocket.sendTXT(num, "Message received");
+    }
+
+    void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
+        switch (type) {
+            case WStype_DISCONNECTED:
+                Serial.printf("[%u] Disconnected!\n", num);
+                break;
+            case WStype_CONNECTED: {
+                IPAddress ip = webSocket.remoteIP(num);
+                Serial.printf("[%u] Connection from %s\n", num, ip.toString().c_str());
+            }
+            break;
+            case WStype_TEXT:
+                handleWebSocketMessage(num, payload, length);
+                break;
+        }
+    }
+};
+
+WebServerManager serverManager("Asuka", "carlosvv");
+
+void setup() {
+    serverManager.begin();
 }
 
-void notFound(AsyncWebServerRequest *request) {
-  request->send(404, "text/plain", "Not found");
-}
-
-void handleWebSocketMessage(uint8_t num, uint8_t *data, size_t len) {
-  data[len] = 0; // Null-terminate the string
-  Serial.printf("WebSocket Message: %s\n", (char *)data);
-
-  // Send a response back to the client
-  webSocket.sendTXT(num, "Message received");
-}
-
-void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
-  switch(type) {
-    case WStype_DISCONNECTED:
-      Serial.printf("[%u] Disconnected!\n", num);
-      break;
-    case WStype_CONNECTED: {
-        IPAddress ip = webSocket.remoteIP(num);
-        Serial.printf("[%u] Connection from %s\n", num, ip.toString().c_str());
-      }
-      break;
-    case WStype_TEXT:
-      handleWebSocketMessage(num, payload, length);
-      break;
-  }
-}
-
-void setup(){
-  // Serial port for debugging purposes
-  Serial.begin(115200);
-
-  // Initialize LittleFS
-  if(!LittleFS.begin()){
-    Serial.println("An Error has occurred while mounting LittleFS");
-    return;
-  }
-
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi");
-
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(LittleFS, "/jerk.html", String(), false, processor);
-  });
-
-  // Route to load JavaScript file
-  server.on("/joy.js", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(LittleFS, "/joy.js", "text/javascript");
-  });
-
-  // Route to load minified JavaScript file
-  server.on("/joy.min.js", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(LittleFS, "/joy.min.js", "text/javascript");
-  });
-
-  // Start server
-  server.begin();
-
-  // Start WebSocket server
-  webSocket.begin();
-  webSocket.onEvent(onWebSocketEvent);
-}
-
-void loop(){
-  webSocket.loop();
+void loop() {
+    serverManager.loop();
 }
